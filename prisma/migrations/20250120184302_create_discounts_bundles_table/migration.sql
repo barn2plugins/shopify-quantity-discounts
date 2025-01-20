6f0e6b14d87ff3f1a939c02af7627dcb881eabda
Ohidul Islam <ohidul.islam951@gmail.com>
2025-01-21 01:08:51 +0600

    Update discount bundles migrations table

--- prisma/migrations/20250120184302_create_discounts_bundles_table/migration.sql
+++ prisma/migrations/20250120184302_create_discounts_bundles_table/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "DiscountBundle" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "previewEnabled" BOOLEAN NOT NULL,
+    "whichProducts" TEXT NOT NULL,
+    "priority" INTEGER NOT NULL DEFAULT 0,
     "sessionId" TEXT NOT NULL,
     "active" BOOLEAN NOT NULL,
-    "previewEnabled" BOOLEAN NOT NULL,
-    "discountType" TEXT NOT NULL,
-    "whichProducts" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     CONSTRAINT "DiscountBundle_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "Session" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
