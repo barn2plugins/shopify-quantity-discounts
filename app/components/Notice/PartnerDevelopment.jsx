d24977adde06870dcb16894bd5ea7d191c78dc09
Ohidul Islam <ohidul.islam951@gmail.com>
2025-01-22 11:27:04 +0600

    Display free plan notice

---
+++ app/components/Notice/PartnerDevelopment.jsx
@@ -0,0 +1,14 @@
+export default function PartnerDevelopment() {
+
+    const styles = {
+        backgroundColor: 'rgb(254 252 232 / var(--tw-bg-opacity, 1))',
+        padding: '.75rem',
+        textAlign: 'center'
+    }
+
+    return (
+        <div style={styles}>
+            <p>This app is free for development stores. When your store goes live, all premium features will be hidden until you choose a plan and approve subscription.</p>
+        </div>
+    )
+}
--- app/root.jsx
+++ app/root.jsx
@@ -4,9 +4,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
 
+// Internal components and libraries
+import PartnerDevelopment from "./components/Notice/PartnerDevelopment";
+import prisma from "./db.server";
+import { authenticate } from "./shopify.server";
+
+export const loader = async ({ request }) => {
+  const { session } = await authenticate.admin(request);
+  const user = await prisma.session.findFirst({
+    where: {
+      id: session.id
+    }
+  });
+  
+  return user;
+};
+
 export default function App() {
+  const user = useLoaderData();
+
   return (
     <html>
       <head>
@@ -21,7 +40,10 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div>
+          { user.isPartnerDevelopment && <PartnerDevelopment/> }
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
