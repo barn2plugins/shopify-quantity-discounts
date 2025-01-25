483b0bf734a6f17c6e56de8056c871fa0b206b07
Ohidul Islam <ohidul.islam951@gmail.com>
2025-01-25 20:28:10 +0600

    Created product exclusion selection component

---
+++ app/routes/app.discount.create/ProductExclusionsModal.jsx
@@ -0,0 +1,132 @@
+// External libraries
+import {
+  Text,
+  ResourceItem,
+  ResourceList,
+  Thumbnail,
+  Filters
+} from "@shopify/polaris";
+
+import { Modal, TitleBar } from "@shopify/app-bridge-react";
+import { ImageIcon } from '@shopify/polaris-icons';
+import { useState, useEffect } from "react";
+
+export default function ProductExclusionsModal({ 
+  fetcher, 
+  selectedProducts, 
+  setSelectedProducts, 
+}) {
+
+  const [ productSearchQuery, setProductSearchQuery ] = useState('');
+  const [ productsLoading, setProductsLoading ] = useState(false);
+  const [ storeProducts, setStoreProducts ] = useState([]);
+
+  const fetchedProducts = fetcher.data?.products;
+
+  const resourceName = {
+    singular: 'product',
+    plural: 'products',
+  };
+
+  const promotedBulkActions = [
+    {
+      content: 'Save',
+      onAction: () => console.log('Todo: implement bulk edit'),
+    },
+  ];
+
+  const handleFiltersQueryChange = (value) => {
+    setProductSearchQuery(value);
+    console.log('handleFiltersQueryChange');
+    setProductsLoading(true);
+  };
+
+  const handleQueryValueRemove = () => {
+    setProductSearchQuery('');
+    console.log('handleQueryValueRemove');
+    setProductsLoading(false);
+  };
+
+  const handleFiltersClearAll = () => {
+    setProductSearchQuery('');
+    console.log('handleQueryValueRemove');
+  };
+
+  const loadProducts = () => {
+    // Set the loader
+    setProductsLoading(true);
+    // Empty the pre-existing and selected products
+    setStoreProducts([]);
+    setSelectedProducts([]);
+
+    // Submit the form to load products
+    fetcher.submit({
+      intent: 'loadProducts',
+      test: 'wow',
+    }, { method: "POST" })
+  }
+
+  useEffect(() => {
+    if (fetchedProducts) {
+      setProductsLoading(false);
+
+      // Format the object to be used in the ResourceList
+      const formattedProducts = fetchedProducts.map(({ node, cursor }) => ({
+        id: node.id.match(/\d+$/)[0],
+        title: node.title,
+        handle: node.handle,
+        imageUrl: node.featuredMedia?.preview?.image?.url,
+        cursor,
+      }))
+
+      setStoreProducts(formattedProducts);
+    }
+  }, [fetchedProducts])
+
+  return (
+    <Modal id="my-modal" variant="base" onShow={loadProducts}>
+      <TitleBar title="Select products"></TitleBar>
+      <div style={{ maxHeight: '600px' }}>
+        <ResourceList
+          resourceName={resourceName}
+          items={storeProducts}
+          renderItem={renderItem}
+          selectedItems={selectedProducts}
+          onSelectionChange={setSelectedProducts}
+          promotedBulkActions={promotedBulkActions}
+          filterControl={
+            <Filters
+              queryValue={productSearchQuery}
+              queryPlaceholder={"Search " + resourceName.plural}
+              filters={[]}
+              onQueryChange={handleFiltersQueryChange}
+              onQueryClear={handleQueryValueRemove}
+              onClearAll={handleFiltersClearAll}
+            />
+          }
+          emptySearchState={<div>This is a custom empty state</div>}
+          selectable
+          loading={productsLoading}
+        />
+      </div>
+    </Modal>
+  )
+
+  function renderItem(item) {
+    const {id, title, imageUrl } = item;
+    const productId = id.match(/\d+$/)[0];
+    
+    return (
+      <ResourceItem
+        id={productId}
+        media={<Thumbnail size="small" alt={title} source={imageUrl ?? ImageIcon} />}
+        accessibilityLabel={`View details for ${title}`}
+        verticalAlignment="center"
+      >
+        <Text variant="bodyMd" as="h3">
+          {title}
+        </Text>
+      </ResourceItem>
+    );
+  }
+}
--- app/routes/app.discount.create/route.jsx
+++ app/routes/app.discount.create/route.jsx
@@ -13,37 +13,35 @@ import {
   BlockStack,
   InlineStack,
   Box,
-  ResourceItem,
-  ResourceList,
-  Thumbnail,
-  Filters
 } from "@shopify/polaris";
 import {
   BookIcon,
   CashPoundIcon,
-  ImageIcon,
   QuestionCircleIcon,
 } from '@shopify/polaris-icons';
-import { json } from "@remix-run/node";
 import { useState, useEffect } from "react";
 import { useLoaderData, useFetcher } from "@remix-run/react";
-import { Modal, TitleBar, useAppBridge } from "@shopify/app-bridge-react";
+import { useAppBridge } from "@shopify/app-bridge-react";
 
 // Internal libraries
 import styles from "./styles.module.css";
 import { authenticate } from "../../shopify.server"
 import prisma from "../../db.server";
 import { fetchProducts, updateDiscount, createDiscountBundle } from "./actions";
+import ProductExclusionsModal from "./ProductExclusionsModal";
 
 export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+  
   let settings = {
+    discountBundleId: null,
     discountName: 'Bundle',
     discountType: 'volume_bundle',
     whichProducts: 'all_products',
     preview: true,
-    active: false
+    enabled: false
   }
-  return json( settings )
+  return settings
 };
 
 export const action = async ({ request }) => {
@@ -51,195 +49,85 @@ export const action = async ({ request }) => {
 
   const formData = await request.formData();
   const fetcherData = Object.fromEntries(formData);
+  console.log(fetcherData);
    
   // Load products from merchants store
   if ( fetcherData.intent === 'loadProducts' ) {
     try {
-      const products = await fetchProducts(admin);
-      return { success: true, products: products.data.products.edges };
+      const { data: { products: { edges } } } = await fetchProducts(admin);
+      return { success: true, products: edges };
     } catch {
       return { success: false, error: "Failed to fetch products" };
     }
   }
 
   // Create or update discount bundle
-  try {
-    const actionIntent = ( fetcherData.intent && fetcherData.intent === 'update' ) || !fetcherData.active ? true : false;
-
-    const discountBundle = actionIntent 
-      ? await updateDiscount({ prisma, fetcherData }) 
-      : await createDiscountBundle({ prisma, fetcherData, session });
-
-    return {
-      success: true,
-      discountBundle: discountBundle
-    };
-  } catch {
-    return { success: false, error: "Failed to create discount bundles" };
+  if ( fetcherData.intent === 'create' || fetcherData.intent === 'update' ) {
+    try {
+      const discountBundle = fetcherData.intent === 'create' 
+        ? await createDiscountBundle({ prisma, fetcherData, session }) 
+        : await updateDiscount({ prisma, fetcherData });
+  
+      return {
+        success: true,
+        discountBundle: discountBundle
+      };
+    } catch {
+      return { success: false, error: "Failed to create discount bundles" };
+    }
   }
+
+  return null;
 }
 
 export default function DiscountPage() {
   const fetcher = useFetcher();
   const shopify = useAppBridge();
   const settings = useLoaderData();
-  const isLoading = ["loading", "submitting"].includes(fetcher.state) && fetcher.formMethod === "POST";
   
   const [ formState, setFormState ] = useState(settings);
-  const [ modalProducts, setModalProducts ] = useState([]);
 
-  const discountId = fetcher.data?.discountBundle?.id;
-  const loadedProducts = fetcher.data?.products;
+  const isLoading = ["loading", "submitting"].includes(fetcher.state) 
+    && fetcher.formMethod === "POST" 
+    && (fetcher.formData?.get("intent") === "create" || fetcher.formData?.get("intent") === "update");
+
+  const discountBundleId = fetcher.data?.discountBundle?.id;
 
   useEffect(() => {
-    if ( discountId ) {
+    if ( discountBundleId ) {
       shopify.toast.show("Discount bundle has been created");
+      setFormState({...formState, discountBundleId: discountBundleId});
     }
-  }, [discountId, shopify]);
-
-  useEffect(() => {
-    if (loadedProducts) {
-      setProductsLoading(false);
-
-      // Format the object to be used in the ResourceList
-      const formattedProducts = loadedProducts.map(({ node, cursor }) => ({
-        id: node.id.match(/\d+$/)[0],
-        title: node.title,
-        handle: node.handle,
-        imageUrl: node.featuredMedia?.preview?.image?.url,
-        cursor,
-      }))
-
-      setModalProducts(formattedProducts);
-    }
-  }, [loadedProducts])
+  }, [discountBundleId, shopify]);
 
   // On clicking the create discount button, set the form state to active and submit the form
-  const createDiscount = () => {
-    setFormState({...formState, active: true});
-    fetcher.submit(formState, { method: "POST" })
+  const createDiscountBundle = () => {
+    const formData = {
+      ...formState,
+      intent: discountBundleId !== undefined ? 'update' : 'create'
+    }
+    console.log('formData');
+    console.log(formData);
+    
+    fetcher.submit(formData, { method: "POST" })
   };
 
   const [selectedProducts, setSelectedProducts] = useState([]);
-  const [productSearchQuery, setProductSearchQuery] = useState('');
-  const [productsLoading, setProductsLoading] = useState(false);
-
-  const resourceName = {
-    singular: 'product',
-    plural: 'products',
-  };
-
-  const loadProducts = () => {
-    // Set the loader
-    setProductsLoading(true);
-
-    // Submit the form to load products
-    fetcher.submit({
-      intent: 'loadProducts',
-    }, { method: "POST" })
-  }
-
-  const handleFiltersQueryChange = (value) => {
-    setProductSearchQuery(value);
-    console.log('handleFiltersQueryChange');
-    setProductsLoading(true);
-  };
-
-  const handleQueryValueRemove = () => {
-    setProductSearchQuery('');
-    console.log('handleQueryValueRemove');
-    setProductsLoading(false);
-  };
-
-  const handleFiltersClearAll = () => {
-    setProductSearchQuery('');
-    console.log('handleQueryValueRemove');
-  };
-
-  const promotedBulkActions = [
-    {
-      content: 'Save',
-      onAction: () => console.log('Todo: implement bulk edit'),
-    },
-  ];
-
-  function renderItem(item) {
-    const {id, title, imageUrl } = item;
-    const productId = id.match(/\d+$/)[0];
-    
-    return (
-      <ResourceItem
-        id={productId}
-        media={<Thumbnail size="small" alt={title} source={imageUrl ?? ImageIcon} />}
-        accessibilityLabel={`View details for ${title}`}
-        verticalAlignment="center"
-      >
-        <Text variant="bodyMd" as="h3">
-          {title}
-        </Text>
-      </ResourceItem>
-    );
-  }
 
   return (
     <div className={styles.pageWrapper}>
       <fetcher.Form method="post">
-        <Modal id="my-modal" variant="base" onShow={loadProducts}>
-          <TitleBar title="Select products"></TitleBar>
-          <div style={{ height: '600px' }}>
-            <ResourceList
-              resourceName={resourceName}
-              items={modalProducts}
-              renderItem={renderItem}
-              selectedItems={selectedProducts}
-              onSelectionChange={setSelectedProducts}
-              promotedBulkActions={promotedBulkActions}
-              filterControl={
-                <Filters
-                  queryValue={productSearchQuery}
-                  queryPlaceholder={"Search " + resourceName.plural}
-                  filters={[]}
-                  onQueryChange={handleFiltersQueryChange}
-                  onQueryClear={handleQueryValueRemove}
-                  onClearAll={handleFiltersClearAll}
-                />
-              }
-              emptySearchState={<div>This is a custom empty state</div>}
-              selectable
-              loading={productsLoading}
-            />
-          </div>
-{/*           
-          <Box style={{
-            padding: '10px',
-            borderTopStyle: 'solid',
-            borderTopWidth: 'var(--p-border-width-025)',
-            borderColor: 'var(--p-color-border)',
-            boxShadow: 'var(--pc-box-shadow)'
-          }}>
-              <InlineStack gap={200} blockAlign="center" align="space-between">
-                <InlineStack gap={200}>
-                  <Text as="p">5 products selected</Text>
-                </InlineStack>
-                <InlineStack gap={200}>
-                  <Button variant="primary" size="micro">Label</Button>
-                  <Button onClick={() => shopify.modal.hide('my-modal')} size="micro">Label</Button>
-                </InlineStack>
-              </InlineStack>
-          </Box> */}
-        </Modal>
         <Page
           fullWidth={true}
           backAction={{content: 'Home', url: '/app'}}
-          title={ fetcher.data?.discountBundle?.id ? 'Update discount' : 'Create discount' }
+          title={ discountBundleId ? 'Update discount' : 'Create discount' }
           primaryAction={
             <Button 
               variant="primary"
-              submit={true}
               loading={isLoading}
-              onClick={createDiscount}
+              onClick={createDiscountBundle}
             >
-                { fetcher.data?.discountBundle?.id ? 'Update' : 'Create' }
+                { discountBundleId ? 'Update' : 'Create' }
             </Button>
           }
         >
@@ -391,14 +279,13 @@ export default function DiscountPage() {
             </Layout.Section>
           </Layout>
         </Page>
-        {/* Hidden fields */}
-        <input type="hidden" name="active" value={formState.active ? true : false} />
-        { discountId && (
-          <>
-            <input type="hidden" name="discountBundleId" value={discountId} />
-            <input type="hidden" name="intent" value="update" />
-          </>
-        ) }
+        
+        <ProductExclusionsModal
+          selectedProducts={selectedProducts}
+          setSelectedProducts={setSelectedProducts}
+          fetcher={fetcher}
+        />
+
       </fetcher.Form>
     </div>
   );
