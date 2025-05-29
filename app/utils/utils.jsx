c456865df175745d2e4973aa521c11dafc0ad930
ohid <ohidul.islam951@gmail.com>
2025-05-29 12:42:48 +0600

    Updated default bulk pricing rules

--- app/utils/utils.jsx
+++ app/utils/utils.jsx
@@ -218,7 +218,7 @@ export const getDefaultPricingTiers = () => {
     },
     {
       min_quantity: 11,
-      max_quantity: 21,
+      max_quantity: 20,
       discount: '',
       discount_type: 'percentage'
     },
